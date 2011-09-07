d3c2ed2f334f5b047cad3c31b8658b621cec5b8c
marco.molinari1980 <marco.molinari1980@gmail.com>
2011-09-07 19:28:58 +0000

    modificati nomi classi table, aggiunta 2d

--- src/grafica/componenti/table/TableScrollPane.java
+++ src/grafica/componenti/table/TableScrollPane.java
@@ -5,19 +5,19 @@ import javax.swing.JScrollPane;
 public class TableScrollPane extends JScrollPane {
 
 	private static final long serialVersionUID = 1L;
-	private TableBase tabellaFiglia;
+	private TableBase2d tabellaFiglia;
 
-	public TableScrollPane(final TableBase tableBase) {
+	public TableScrollPane(final TableBase2d tableBase) {
 		super(tableBase);
 		this.tabellaFiglia = tableBase;
 		this.setBounds(tableBase.getX(), tableBase.getY(), tableBase.getWidth(), tableBase.getHeight());
 	}
 
-	public TableBase getTabellaFiglia() {
+	public TableBase2d getTabellaFiglia() {
 		return tabellaFiglia;
 	}
 
-	public void setTabellaFiglia(TableBase tabellaFiglia) {
+	public void setTabellaFiglia(TableBase2d tabellaFiglia) {
 		this.tabellaFiglia = tabellaFiglia;
 	}
 }
