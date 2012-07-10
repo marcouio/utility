2657c8f940fe1a6293889dcd2620196ee60cf046
marco.molinari1980@gmail.com <marco.molinari1980@gmail.com@8be8bb78-a580-ef64-62f8-33edb657bb8e>
2012-07-10 18:24:20 +0000

    modifiche marginali

--- trunk/Utility/src/grafica/componenti/componenteBase/ComponenteBaseConPadreContenitore.java
+++ trunk/Utility/src/grafica/componenti/componenteBase/ComponenteBaseConPadreContenitore.java
@@ -13,12 +13,12 @@ public class ComponenteBaseConPadreContenitore extends ComponenteBase {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public void aggiungiAlContenitore(final Container contenitorePadre2, final Component componenteFiglio) {
+	public void aggiungiAlContenitore(final Container contenitorePadre, final Component componenteFiglio) {
 		Container cont = null;
-		if (contenitorePadre2 != null && componenteFiglio != null) {
-			cont = contenitorePadre2;
+		if (contenitorePadre != null && componenteFiglio != null) {
+			cont = contenitorePadre;
 			super.aggiungiAlContenitore(cont, componenteFiglio);
-		} else if (contenitorePadre2 == null && !(componenteFiglio instanceof DialogoBase || componenteFiglio instanceof FrameBase)) {
+		} else if (contenitorePadre == null && !(componenteFiglio instanceof DialogoBase || componenteFiglio instanceof FrameBase)) {
 			System.err.println("Si sta cercando di inserire il componente in un contenitore nullo. Componente: " + componenteFiglio);
 			LineStackTracePrinter.scriviLineeErrore();
 		} else if (componenteFiglio == null) {
