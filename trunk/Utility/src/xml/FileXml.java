3946effb33bbf8222a10e9d237a1519f55a4abac
marco.molinari1980@gmail.com <marco.molinari1980@gmail.com@8be8bb78-a580-ef64-62f8-33edb657bb8e>
2013-01-30 08:46:35 +0000

    mod

---
+++ trunk/Utility/src/xml/FileXml.java
@@ -0,0 +1,49 @@
+package xml;
+
+import java.io.File;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class FileXml {
+
+	private File file;
+	private Document doc;
+
+	public FileXml(final String path) throws Exception {
+		file = new File(path);
+		doc = UtilXml.createDocument(file);
+
+	}
+
+	public void addRootElement(final String nomeRoot) {
+		UtilXml.addRootElement(doc, nomeRoot);
+	}
+
+	public void addElement(final String elementoPadre, final String nomeElemento) {
+		final Element elementById = getDoc().getElementById(elementoPadre);
+		UtilXml.addElement(getDoc(), elementById, nomeElemento);
+	}
+
+	public void addAttribute(final String elementoPadre, final String nomeElemento, final String valueElemento) {
+		final Element elementById = getDoc().getElementById(elementoPadre);
+		UtilXml.addAttribute(getDoc(), elementById, nomeElemento, valueElemento);
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(final File file) {
+		this.file = file;
+	}
+
+	public Document getDoc() {
+		return doc;
+	}
+
+	public void setDoc(final Document doc) {
+		this.doc = doc;
+	}
+
+}
