8a8f6935766ac9e644b7226c030e8bc6d39c6008
Marco Molinari <marco.molinari1980@gmail.com>
2017-03-18 19:17:00 +0100

    moved

--- src/main/java/com/molinari/utility/commands/UndoCommand.java
+++ src/main/java/com/molinari/utility/commands/UndoCommand.java
@@ -3,28 +3,7 @@ package com.molinari.utility.commands;
 public class UndoCommand extends AbstractCommand {
 
 	@Override
-	public boolean execute() {
-		return true;
-	}
-
-	@Override
-	public boolean unExecute() {
-		return true;
-	}
-
-	@Override
 	public String toString() {
 		return "Effettuato comando 'Indietro'";
 	}
-
-	@Override
-	public void scriviLogExecute(final boolean isComandoEseguito) {
-
-	}
-
-	@Override
-	public void scriviLogUnExecute(final boolean isComandoEseguito) {
-
-	}
-
 }
