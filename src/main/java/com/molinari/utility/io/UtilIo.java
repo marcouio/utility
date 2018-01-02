819fc8eaa43c357175b3dcb481e41ffd4c7e4665
Marco Molinari <marco.molinari1980@gmail.com>
2018-01-03 00:28:18 +0100

    SONAR

--- src/main/java/com/molinari/utility/io/UtilIo.java
+++ src/main/java/com/molinari/utility/io/UtilIo.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.file.CopyOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,25 +30,25 @@ import com.molinari.utility.controller.ControlloreBase;
 
 public class UtilIo {
 
-	public static void uploadFile(String hostAddr, int port, String fileName) throws IOException { 
-		final byte[] buf = new byte[1000]; 
-		int len; 
-		// connessione con l'host, ad una data porta 
-		final Socket sock = createSock(hostAddr, port); 
-		final FileInputStream in = createFileInputStream(fileName); 
-		final OutputStream out = sock.getOutputStream(); 
-		while ( ( len = in.read(buf) ) != -1 ) { 
-			out.write(buf, 0, len); 
-			out.flush(); 
-		} 
-		out.close(); 
-		sock.close(); 
+	public static void uploadFile(String hostAddr, int port, String fileName) throws IOException {
+		final byte[] buf = new byte[1000];
+		int len;
+		// connessione con l'host, ad una data porta
+		final Socket sock = createSock(hostAddr, port);
+		final FileInputStream in = createFileInputStream(fileName);
+		final OutputStream out = sock.getOutputStream();
+		while ((len = in.read(buf)) != -1) {
+			out.write(buf, 0, len);
+			out.flush();
+		}
+		out.close();
+		sock.close();
 		in.close();
 	}
-	
-	public static String prepareUrl(String pathPar){
+
+	public static String prepareUrl(String pathPar) {
 		String path = pathPar;
-		if(path != null){
+		if (path != null) {
 			path = adjust(path);
 			return URLUTF8Encoder.unescape(URLUTF8Encoder.encode(path));
 		}
@@ -75,70 +74,65 @@ public class UtilIo {
 	private static Socket createSock(String hostAddr, int port) throws IOException {
 		return new Socket(hostAddr, port);
 	}
-	
-	 /**
-     * Unzip it
-     * @param zipFile input zip file
-     * @param output zip file output folder
-     */
-    public static void unZipIt(String zipFile, String outputFolder){
 
-     byte[] buffer = new byte[1024];
+	/**
+	 * Unzip it
+	 * 
+	 * @param zipFile
+	 *            input zip file
+	 * @param output
+	 *            zip file output folder
+	 */
+	public static void unZipIt(String zipFile, String outputFolder) {
 
-     try{
+		byte[] buffer = new byte[1024];
 
-    	//create output directory is not exists
-    	File folder = new File(outputFolder);
-    	if(!folder.exists()){
-    		folder.mkdir();
-    	}
+		try (FileInputStream fis = new FileInputStream(zipFile); ZipInputStream zis = new ZipInputStream(fis);) {
 
-    	//get the zip file content
-    	ZipInputStream zis =
-    		new ZipInputStream(new FileInputStream(zipFile));
-    	//get the zipped file list entry
-    	ZipEntry ze = zis.getNextEntry();
+			// create output directory is not exists
+			File folder = new File(outputFolder);
+			if (!folder.exists()) {
+				folder.mkdir();
+			}
 
-    	while(ze!=null){
+			// get the zip file content
+			// get the zipped file list entry
+			ZipEntry ze = zis.getNextEntry();
 
-    	   String fileName = ze.getName();
-           File newFile = new File(outputFolder + File.separator + fileName);
+			while (ze != null) {
 
-           System.out.println("file unzip : "+ newFile.getAbsoluteFile());
+				String fileName = ze.getName();
+				File newFile = new File(outputFolder + File.separator + fileName);
 
-            //create all non exists folders
-            //else you will hit FileNotFoundException for compressed folder
-            new File(newFile.getParent()).mkdirs();
+				ControlloreBase.getLog().info(() -> "file unzip : " + newFile.getAbsoluteFile());
 
-            FileOutputStream fos = new FileOutputStream(newFile);
+				// create all non exists folders
+				// else you will hit FileNotFoundException for compressed folder
+				new File(newFile.getParent()).mkdirs();
 
-            int len;
-            while ((len = zis.read(buffer)) > 0) {
-       		fos.write(buffer, 0, len);
-            }
+				try (FileOutputStream fos = new FileOutputStream(newFile);) {
 
-            fos.close();
-            ze = zis.getNextEntry();
-    	}
+					int len;
+					while ((len = zis.read(buffer)) > 0) {
+						fos.write(buffer, 0, len);
+					}
 
-        zis.closeEntry();
-    	zis.close();
+					ze = zis.getNextEntry();
+				}
+			}
 
-    	System.out.println("Done");
+		} catch (IOException ex) {
+			ControlloreBase.getLog().log(Level.SEVERE, ex.getMessage(), ex);
+		}
+	}
 
-    }catch(IOException ex){
-       ex.printStackTrace();
-    }
-   }
+	public static void createZipFile(final String inputFileName, final String zipFileName) throws IOException {
 
-	public static void createZipFile(final String inputFileName,
-			final String zipFileName) {
-
-		try {
-
-			// Creare gli stream d’input e output
-			final FileInputStream inStream = createFileInputStream(inputFileName);
-			final ZipOutputStream outStream = new ZipOutputStream(createFileOutputStream(zipFileName));
+		try (
+				// Creare gli stream d’input e output
+				FileInputStream inStream = createFileInputStream(inputFileName);
+				FileOutputStream fis = createFileOutputStream(zipFileName);
+				ZipOutputStream outStream = new ZipOutputStream(fis);) {
 
 			// Aggiungere un oggetto ZipEntry allo stream d’output
 			outStream.putNextEntry(new ZipEntry(inputFileName));
@@ -153,9 +147,6 @@ public class UtilIo {
 			}
 
 			outStream.closeEntry();
-
-			outStream.close();
-			inStream.close();
 
 		} catch (final IOException ex) {
 			ControlloreBase.getLog().log(Level.SEVERE, ex.getMessage(), ex);
@@ -173,28 +164,28 @@ public class UtilIo {
 	public static void deleteFileDaDirectory(final String directory) {
 		final Predicate<? super File> predicate = File::isFile;
 		deleteFileFromDirectory(directory, predicate);
-		
+
 	}
-	
-	public static void deleteFileFromDirectory(final String directory, Predicate<? super File> predicate){
+
+	public static void deleteFileFromDirectory(final String directory, Predicate<? super File> predicate) {
 		final File dir = new File(directory);
-		
+
 		final File[] listFiles = dir.listFiles();
 		final List<File> fileList = Arrays.asList(listFiles);
 		final Stream<File> streamFiltered = fileList.stream().filter(predicate);
 		streamFiltered.forEach(File::delete);
-			
+
 	}
 
 	public static void deleteFileDaDirectory(final String directory, final String treCharIniziali) {
-		
+
 		final Predicate<? super File> predicate = t -> {
 			final boolean startWith = t.getName().substring(0, 3).equals(treCharIniziali);
 			return t.isFile() && startWith;
 		};
-		
+
 		deleteFileFromDirectory(directory, predicate);
-		
+
 	}
 
 	public static void scriviFileSuPiuRighe(final File file, final List<String> righe) {
@@ -211,27 +202,25 @@ public class UtilIo {
 		}
 	}
 
-	private static FileWriter createFileWriter(final File file)
-			throws IOException {
+	private static FileWriter createFileWriter(final File file) throws IOException {
 		return new FileWriter(file);
 	}
-	
-	public static void close(Closeable ...closeables) throws IOException {
+
+	public static void close(Closeable... closeables) throws IOException {
 		for (final Closeable closeable : closeables) {
 			closeable.close();
 		}
-		
 	}
-	
-	public abstract class Reader<T>{
-	
+
+	public abstract class Reader<T> {
+
 		private final File file;
 
 		public Reader(File file) {
 			this.file = file;
 		}
-		
-		public List<T> readAll() throws FileNotFoundException{
+
+		public List<T> readAll() throws FileNotFoundException {
 			final List<T> result = new ArrayList<>();
 			final FileInputStream fis = new FileInputStream(file);
 			try (Scanner scanner = new Scanner(new InputStreamReader(fis, UTF_8))) {
@@ -247,10 +236,9 @@ public class UtilIo {
 	}
 
 	public static boolean check(final String estensione, final File file) {
-		if(fileExist(file)){ 
-				return checkEstensione(estensione, file)
-				&& checkLunghezzaNome(estensione, file)
-				&& checkAssenzaParentesi(file);
+		if (fileExist(file)) {
+			return checkEstensione(estensione, file) && checkLunghezzaNome(estensione, file)
+					&& checkAssenzaParentesi(file);
 		}
 		return false;
 	}
@@ -277,11 +265,11 @@ public class UtilIo {
 		return mp3.renameTo(file2);
 	}
 
-	public static boolean moveFile(final File origine, final File destinazione, CopyOption... copyOptions) {
+	public static boolean moveFile(final File origine, final File destinazione) {
 		String pathTo = prepareUrl(destinazione.getAbsolutePath());
 		try {
 			Files.move(origine, new File(pathTo));
-			return  true;
+			return true;
 		} catch (IOException e) {
 			throw new GenericException(e);
 		}
